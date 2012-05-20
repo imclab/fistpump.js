bb15d97a9feae7102ff991bbf5281744f3ecb4ee
Abe Stanway <abestanway@gmail.com>
2012-05-19 22:58:47 -0500

    Server stuff

--- server.js
+++ server.js
@@ -8,9 +8,34 @@ app.configure(function() {
   app.use(express.static('./public'));
 });
 
+
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  socket.on('clientVelocity', function(data){
+    if(data.b > 0 && Math.abs(data.g) < 70){
+      angle = data.b
+      data['angle'] = angle
+      socket.broadcast.emit('fuckingVelocity', data)
+    }
+
+    if(data.b > 0 && Math.abs(data.g) >= 70){
+      angle = 180 - data.b
+      data['angle'] = angle
+      socket.broadcast.emit('fuckingVelocity', data)
+    }
+
+    if(data.b < 0){
+      angle = Math.abs(data.b)
+      data['angle'] = angle
+      socket.broadcast.emit('fuckingVelocity', data)
+    }
+  })
+
+  socket.on('wantVelocity', function(data){
+      socket.broadcast.emit('getVelocity')
+  })
+
+  socket.on('gotVelocity', function(data){
+    console.log("WOOOO")
+    socket.broadcast.emit('browserVolume', data)
   });
 });
