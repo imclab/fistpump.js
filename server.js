2a6bd8ee4e1af4c316f550ee1131ad374251a95a
Artem Titoulenko <artem.titoulenko@gmail.com>
2012-05-19 23:04:04 -0400

    better framework. clientside should be in index.htm

--- server.js
+++ server.js
@@ -1,25 +1,44 @@
-var express = require('express');
-var app = express.createServer();
-
-var couch = require('couch-client'),
-		db = couch('http://moxie.iriscouch.com/**DB_NAME**'); //your couchdb server here
-
-// set rendering views
-app.set('views',__dirname + '/views');
-app.set('view engine','jade');
-app.use(express.bodyParser());
-app.use(express.cookieParser());
-app.use(express.static(__dirname + '/public'));
-
-app.get('/',function(req,res){
-	db.save({
-		user: req.connection.remoteAddress,
-		action: 'visit',
-		date: Date()
-	}, function(err,doc){
-		if(err) console.log(err);
-	});
-	res.render('main',{layout: false})
-});
-
-app.listen(3000);
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+ 
+http.createServer(function (request, response) {
+      
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.htm';
+         
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+     
+    path.exists(filePath, function(exists) {
+     
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+     
+}).listen(3000);
+ 
+console.log('Server running at http://127.0.0.1:3000/');
